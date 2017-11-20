5f12c7b8d977f79fdea85cfff5697c1b750c1ab5
SlavniyTeo <slavniyteo@gmail.com>
2017-11-20 12:58:28 +0300

    new preview

--- Assets/Raw/Scripts/Editor/TriangulatePreviewEditor.cs
+++ Assets/Raw/Scripts/Editor/TriangulatePreviewEditor.cs
@@ -10,12 +10,32 @@ using TriangleNet.Geometry;
 [CustomEditor(typeof(TriangulatePreview))]
 public class TriangulatePreviewEditor : Editor {
 
+	private bool drawFullPreview = false;
+
 	private new TriangulatePreview target { get { return base.target as TriangulatePreview; } }
 
-	public override void DrawPreview(Rect rect){
+	private void DrawMeshPreview(Rect rect){
 		EditorGUI.DrawRect(rect, Color.gray);
 
-		var mesh = target.GenerateMesh(rect);
+		var mesh = GenerateMesh(rect, target.circuit);
+
+		if (drawFullPreview){
+			DrawFullPreview(rect, mesh);
+		}
+		else {
+			DrawSimplePreview(rect, mesh);
+		}
+	}
+
+	private void DrawSimplePreview(Rect rect, TriangleNet.Mesh mesh){
+		foreach (var s in mesh.Segments){
+			if (s.GetTriangle(0) == null || s.GetTriangle(1) == null){
+				DrawLine(rect, s.GetVertex(0), s.GetVertex(1), Color.blue);
+			}
+		}
+	}
+
+	private void DrawFullPreview(Rect rect, TriangleNet.Mesh mesh){
 		foreach (var t in mesh.Triangles){
 			DrawLine(rect, t.GetVertex(0), t.GetVertex(1), Color.blue);
 			DrawLine(rect, t.GetVertex(0), t.GetVertex(2), Color.blue);
@@ -28,12 +48,47 @@ public class TriangulatePreviewEditor : Editor {
 				bounds.Add(s);
 			}
 		}
-		for (int i = 0; i < Mathf.Min(10000, bounds.Count); i++){
+		for (int i = 0; i < Mathf.Min(1000, bounds.Count); i++){
 			var s = bounds[i];
 			var color = new Color((float)i / bounds.Count, 0,0,1);
 			DrawLine(rect, s.GetVertex(0), s.GetVertex(1), color);
 			DrawVerticle(rect, s.GetVertex(0), color);
 		}
+	}
+
+	public TriangleNet.Mesh GenerateMesh(Rect bounds, Vector2[] circuit) {
+		var verticles = NormilizedVerticles(circuit, bounds);
+
+		var geometry = new TriangleNet.Geometry.InputGeometry(verticles.Count);
+		verticles.ForEach(v => geometry.AddPoint(v.x, v.y));
+		for (int i = 0; i < verticles.Count - 1; i++){
+			geometry.AddSegment(i, i + 1, 1);
+		}
+		geometry.AddSegment(verticles.Count - 1, 0, 1);
+
+		var mesh = new TriangleNet.Mesh(new TriangleNet.Behavior{
+			ConformingDelaunay = true,
+			NoBisect = 0
+		});
+		mesh.Triangulate(geometry);
+
+		return mesh;
+	}
+
+	private List<Vector2> NormilizedVerticles(Vector2[] verticles, Rect rect){
+		List<Vector2> result = new List<Vector2>();
+
+		var min = verticles.Aggregate((v, res) => new Vector2(Mathf.Min(v.x, res.x), Mathf.Min(v.y, res.y)));
+		var max = verticles.Aggregate((v, res) => new Vector2(Mathf.Max(v.x, res.x), Mathf.Max(v.y, res.y)));
+		foreach (var vertex in verticles){
+			var v = (vertex - min);
+			v.y = (v.y / max.y) * (rect.height - 10);
+			v.x = (v.x / max.x) * (rect.width - 10);
+
+			result.Add(v);
+		}
+
+		return result;
 	}
 
 	private void DrawVerticle(Rect rect, Vertex verticle, Color color){
@@ -46,15 +101,36 @@ public class TriangulatePreviewEditor : Editor {
 	}
 
 	private Vector2 ToVector2(Rect rect, TriangleNet.Data.Vertex vertex){
-		return rect.position + new Vector2((float)vertex.X, (float)vertex.Y) + Vector2.one * 0.25f;
+		return rect.position + new Vector2((float)vertex.X, rect.height - (float)vertex.Y) + Vector2.one * 0.25f;
 	}
 
+	private Vector2 ToVector2(Rect rect, Vector2 position){
+		return rect.position + new Vector2(position.x, rect.height - position.y) + Vector2.one * 0.25f;
+	}
+
+	private void DrawCircuit(Rect rect){
+		EditorGUI.DrawRect(rect, Color.gray);
+
+		var circuit = NormilizedVerticles(target.circuit, rect);
+		for (int i = 1; i < circuit.Count; i++){
+			Drawing.DrawLine(ToVector2(rect, circuit[i - 1]), ToVector2(rect,circuit[i]), Color.red, 2, false);
+		}
+	}
 
 	public override void OnInspectorGUI(){
 		DrawDefaultInspector();
 
+		EditorGUILayout.Separator();
+		if (GUILayout.Button(drawFullPreview ? "Draw simple preview" : "Draw full preview")){
+			drawFullPreview = ! drawFullPreview;
+		}
+
 		var rect = GUILayoutUtility.GetAspectRect(1);
-		DrawPreview(rect);
+		DrawMeshPreview(rect);
+
+		EditorGUILayout.Separator();
+		var circuitRect = GUILayoutUtility.GetAspectRect(1);
+		DrawCircuit(circuitRect);
 
 	}
 }
--- Assets/Raw/Scripts/TriangulatePreview.cs
+++ Assets/Raw/Scripts/TriangulatePreview.cs
@@ -11,39 +11,4 @@ public class TriangulatePreview : ScriptableObject {
 	public Vector2[] equalDistance;
 	public Vector2[] circuit;
 
-	public TriangleNet.Mesh GenerateMesh(Rect bounds) {
-		var verticles = NormilizedVerticles(this.verticles, bounds);
-
-		var geometry = new TriangleNet.Geometry.InputGeometry(verticles.Count);
-		verticles.ForEach(v => geometry.AddPoint(v.x, v.y));
-		for (int i = 0; i < verticles.Count - 1; i++){
-			geometry.AddSegment(i, i + 1, 1);
-		}
-		geometry.AddSegment(verticles.Count - 1, 0, 1);
-
-		var mesh = new TriangleNet.Mesh(new TriangleNet.Behavior{
-			ConformingDelaunay = true,
-			NoBisect = 0
-		});
-		mesh.Triangulate(geometry);
-
-		return mesh;
-	}
-
-	private List<Vector2> NormilizedVerticles(Vector2[] verticles, Rect rect){
-		List<Vector2> result = new List<Vector2>();
-
-		var min = verticles.Aggregate((v, res) => new Vector2(Mathf.Min(v.x, res.x), Mathf.Min(v.y, res.y)));
-		var max = verticles.Aggregate((v, res) => new Vector2(Mathf.Max(v.x, res.x), Mathf.Max(v.y, res.y)));
-		foreach (var vertex in verticles){
-			var v = (vertex - min);
-			v.y = (v.y / max.y) * (rect.height - 10);
-			v.x = (v.x / max.x) * (rect.width - 10);
-
-			result.Add(v);
-		}
-
-		return result;
-	}
-
 }
